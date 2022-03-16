f9761758f7cf24d4c1be241cf1ce040a88989818
Paulo Perez <pauloperez@bittronic.net>
2022-03-15 23:25:52 -0500

    validate function_exists

--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,14 @@
 <?php
 
-function upper($value)
-{
+if(! function_exists('upper')){
+    function upper($value)
+    {
     return Text\Format::upperText($value);
+    }
 }
-function lower($value)
-{
-    return Text\Format::lowerText($value);
+if(! function_exists('lower')){
+    function lower($value)
+    {
+        return Text\Format::lowerText($value);
+    }
 }
