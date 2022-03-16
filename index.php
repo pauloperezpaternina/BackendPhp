1e97c69087f4d695195532026cbf13c6697289c0
Paulo Perez <pauloperez@bittronic.net>
2022-03-15 23:22:12 -0500

    Lower function created

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-echo Text\Format::upperText('hola');
+echo upper('Hola');
+echo lower(' Mundo');
--- src/Format.php
+++ src/Format.php
@@ -8,4 +8,8 @@ class Format
     {
         return strtoupper($value);
     }
+    Public static function lowerText($value)
+    {
+        return strtolower($value);
+    }
 }
